dcad4e4546a743c89f56557c72e07193a2e3c1df
gabrielcarvalhoc <gabrielcarvalho@poli.ufrj.br>
2021-07-08 15:04:21 -0300

    finalizar projeto hora do dia

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hora do Dia :sunny::crescent_moon:
+
+Seja bem vindo! Neste projeto, criei um um site que nos mostra o horário e. de acordo com o período do dia, ele muda a sua aparência!
+
+Para a elaboração do projeto, utilizei:
+
+- **HTML**
+- **CSS**
+
+- **JavaScript**
+
+Fique à vontade para conferir o resultado neste repositório e para entrar em contato.
+
+## 🚀
+
---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+
+    if (hora >= 6 && hora < 12) {
+        msg.innerHTML = `Bom dia, agora são ${hora} hora(s) e ${minuto} minuto(s).`;
+        img.src = 'img/manha.jpg';
+        document.body.style.background = '#bdb72f';
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = `Boa tarde, agora são ${hora} hora(s) e ${minuto} minuto(s).`;
+        img.src = 'img/tarde.jpg';
+        document.body.style.background = '#c0eaf8';
+    } else {
+        msg.innerHTML = `Boa noite, agora são ${hora} hora(s) e ${minuto} minuto(s).`;
+        img.src = 'img/noite.jpg';
+        document.body.style.background = '#263650';
+    }
+}
