334e58fceaffae30320c77a61a47d589d9eda2e1
Gabriel Carvalho <82124316+gabrielcarvalhoc@users.noreply.github.com>
2021-07-12 17:14:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function carregar() {
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
     var data = new Date();
     var hora = data.getHours();
     var minuto = data.getMinutes();
